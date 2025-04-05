6cd6540b74633c3c4fe58b1a16dd21218ffe2717
William Greig <wpgzmk@virginia.edu>
2025-04-05 15:27:39 -0400

    bizness dev

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-terraform {
+ terraform {
+  
   backend "s3" {
     bucket = "saas-workflow-backend"
     key    = "terraform-backend"
     region = "us-east-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws-region
 }
